dbf0f2a93120562f330e04220cdb95e37982784b
Atharva Sawleshwarkar <atharvams009@gmail.com>
2025-03-01 01:23:30 +0530

    dummy data added and cards created

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,9 +1,29 @@
-import ProjectCard from "./ui/project-card"
+import ProjectCard from "./ui/project-card";
+import data from "../data/projects.json";
 
 function Projects() {
   return (
-    <ProjectCard/>
-  )
+    <>
+      <h1 className="text-6xl font-[family-name:var(--font-space-grotsk)] font-[300] mb-3">
+        Worked On
+      </h1>
+      {data.map((card) => (
+        <ProjectCard
+        key={card.description}
+          title={card.title}
+          type={card.type}
+          imgSrc={card.imgSrc}
+          techStack={card.techStack}
+          event={card.event}
+          year={card.year}
+          description={card.description}
+          liveLink={card.liveLink}
+          githubLink={card.githubLink}
+          docker={card.docker}
+        />
+      ))}
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
